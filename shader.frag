debd7121514bea5fdd62cc033436f97867fb21e3
Glitch (salty-saturday) <none>
2019-11-30 16:16:54 +0000

    Ready

--- shader.frag
+++ shader.frag
@@ -11,6 +11,11 @@ uniform sampler2D tex0;
 // mic level from p5
 uniform float mic;
 
+uniform float bass;
+uniform float mid;
+uniform float treble;
+
+
 
 void main() {
   vec2 uv = vTexCoord;
@@ -22,14 +27,14 @@ void main() {
 
   float gray = (tex.r + tex.g + tex.b) / 3.0;
 
-  float res = 40.0;
-  float scl = res / (10.0);
+  float res = 11.0;
+  float scl = res / (5.0);
 
-  float threshR = (fract(floor(tex.r*res)/scl)*scl) * gray;
-  float threshG = (fract(floor(tex.g*res)/scl)*scl) * gray ;
-  float threshB = (fract(floor(tex.b*res)/scl)*scl) * gray;
-  vec3 thresh = vec3(threshR, threshG, threshB*1.9);
+  float threshR = (fract(floor(tex.r*res)/scl)*scl) * gray*bass*mic;
+  float threshG = (fract(floor(tex.g*res)/scl)*scl) * gray*mid;
+  float threshB = (fract(floor(tex.b*res)/scl)*scl) * gray*treble;
+  vec3 thresh = vec3(threshR, threshG, threshB);
 
   // render the output
-  gl_FragColor = vec4(thresh, 1.0);
+  gl_FragColor = vec4(thresh,1.0);
 }
--- sketch.js
+++ sketch.js
@@ -1,40 +1,68 @@
-
- // this variable will hold our shader object
- let theShader;
- // this variable will hold our webcam video
- let cam;
-  let mic;
+// this variable will hold our shader object
+let theShader;
+// this variable will hold our webcam video
+let cam;
+let mic;
+let fft;
 let micLevel;
- function preload(){
-   // load the shader
-   theShader = loadShader('shader.vert', 'shader.frag');
- }
+let bass;
+let mid;
+let treble;
+function preload() {
+  // load the shader
+  theShader = loadShader("shader.vert", "shader.frag");
+}
 
- function setup() {
-   // shaders require WEBGL mode to work
-   createCanvas(710, 400, WEBGL);
-   noStroke();
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(710, 400, WEBGL);
 
-   cam = createCapture(VIDEO);
-   cam.size(710, 400);
-   
-  mic = new p5.AudioIn()
+  // noStroke();
+
+  // initialize the createGraphics layers
+  shaderTexture = createGraphics(710, 400, WEBGL);
+  
+  
+  // turn off the createGraphics layers stroke
+  shaderTexture.noStroke();
+
+  cam = createCapture(VIDEO);
+  cam.size(710, 400);
+
+  mic = new p5.AudioIn();
   mic.start();
+  
+  fft = new p5.FFT();
+  fft.setInput(mic);
+  
+  cam.hide();
+}
+
+function draw() {
+
+  
+  // shader() sets the active shader with our shader
+  shader(theShader);
+
+  // get micLevel
   micLevel = mic.getLevel();
-   
-   cam.hide();
- }
-
- function draw() {
-   // shader() sets the active shader with our shader
-   shader(theShader);
-
-   // passing cam as a texture
-   theShader.setUniform('tex0', cam);
-   
-   // pass mic as float
-   theShader.setUniform('mic', micLevel);
-
-   // rect gives us some geometry on the screen
-   rect(0,0,width,height);
- }
+  
+  micLevel = Math.min(micLevel * 3, 0.999);
+  
+  fft.analyze();
+  bass = fft.getEnergy("bass")/255;
+  mid = fft.getEnergy("mid")/255;
+  treble = fft.getEnergy("treble")/255;
+  
+  treble = Math.sqrt(treble);
+  
+  // Uniforms 
+  theShader.setUniform("tex0", cam);
+  theShader.setUniform("mic", micLevel);
+  theShader.setUniform("bass", bass);
+  theShader.setUniform("mid", mid);
+  theShader.setUniform("treble", treble);
+  
+  
+  rect(mouseX, mouseY, 100, 100);
+}
